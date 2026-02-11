c59b2284c314d622167b3a3ebb4e4eeb8e67821c
Zenithal <i@zenithal.me>
2026-02-11 10:04:35 +0800

    Update noise log

--- log/noise-all.sh
+++ log/noise-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-OMP_NUM_THREADS=16 ./bin/examples/pke/z-noise-test 64 bench | tee noise-64.log
-OMP_NUM_THREADS=16 ./bin/examples/pke/z-noise-test 32 bench | tee noise-32.log
-OMP_NUM_THREADS=16 ./bin/examples/pke/z-noise-test 16 bench | tee noise-16.log
-OMP_NUM_THREADS=16 ./bin/examples/pke/z-noise-test 8 bench | tee noise-8.log
-OMP_NUM_THREADS=16 ./bin/examples/pke/z-noise-test 128 bench | tee noise-128.log
-OMP_NUM_THREADS=16 ./bin/examples/pke/z-noise-test 256 bench | tee noise-256.log
+OMP_NUM_THREADS=16 ./bin/examples/pke/noise-test 64 bench | tee noise-64.log
+OMP_NUM_THREADS=16 ./bin/examples/pke/noise-test 32 bench | tee noise-32.log
+OMP_NUM_THREADS=16 ./bin/examples/pke/noise-test 16 bench | tee noise-16.log
+OMP_NUM_THREADS=16 ./bin/examples/pke/noise-test 8 bench | tee noise-8.log
+OMP_NUM_THREADS=16 ./bin/examples/pke/noise-test 128 bench | tee noise-128.log
+OMP_NUM_THREADS=16 ./bin/examples/pke/noise-test 256 bench | tee noise-256.log
--- log/run-all.sh
+++ log/run-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-OMP_NUM_THREADS=1 ./bin/examples/pke/z-benchmark-full 64 bench | tee bench-64.log
-OMP_NUM_THREADS=1 ./bin/examples/pke/z-benchmark-full 32 bench | tee bench-32.log
-OMP_NUM_THREADS=1 ./bin/examples/pke/z-benchmark-full 16 bench | tee bench-16.log
-OMP_NUM_THREADS=1 ./bin/examples/pke/z-benchmark-full 8 bench | tee bench-8.log
-OMP_NUM_THREADS=1 ./bin/examples/pke/z-benchmark-full 128 bench | tee bench-128.log
-OMP_NUM_THREADS=1 ./bin/examples/pke/z-benchmark-full 256 bench | tee bench-256.log
+OMP_NUM_THREADS=1 ./bin/examples/pke/benchmark-full 64 bench | tee bench-64.log
+OMP_NUM_THREADS=1 ./bin/examples/pke/benchmark-full 32 bench | tee bench-32.log
+OMP_NUM_THREADS=1 ./bin/examples/pke/benchmark-full 16 bench | tee bench-16.log
+OMP_NUM_THREADS=1 ./bin/examples/pke/benchmark-full 8 bench | tee bench-8.log
+OMP_NUM_THREADS=1 ./bin/examples/pke/benchmark-full 128 bench | tee bench-128.log
+OMP_NUM_THREADS=1 ./bin/examples/pke/benchmark-full 256 bench | tee bench-256.log
